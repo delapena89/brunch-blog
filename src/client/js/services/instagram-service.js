204afddb040259138049c1ed004f80c2f5a350b2
Luis <delapenl@gmail.com>
2015-11-10 14:02:13 -0700

    refactored add.html and started looking at instagram api

---
+++ src/client/js/services/instagram-service.js
@@ -0,0 +1,15 @@
+angular.module('brunchBlog')
+  .factory('instagram',instagramFactory);
+
+  instagramFactory.$inject = ['$http'];
+
+  function instagramFactory($http){
+
+
+    var factory = {};
+
+    factory.getInstagram = function(){
+
+    }
+
+  });
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var key = require('../../../_config.js');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+https://api.instagram.com/v1/users/self/feed?access_token=61f8b631abd34732a3bcd8c73d0d73a9
+
+router.post('/api/instagram', function(req, res) {
+  request({
+    method: 'GET',
+    url: "https://api.instagram.com/v1/users/self/feed?access_token="+key.instagramKey
+  }, function(err, response){
+    if(err){
+      console.log('err', err);
+      res.json(err);
+    } else {
+      res.json(response);
+    }
+  });
+});
+
+
+
 
 module.exports = router;
